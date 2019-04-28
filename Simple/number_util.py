7621f1e37e63d45f78e2b0b70ab37d08f6e35ce2
TommorowIsBetter <841331749@qq.com>
2019-04-28 13:00:33 +0800

    Initial commit, add number_util.py

---
+++ Simple/number_util.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author:Wang Yan
+@ide:PyCharm
+@time:2019/4/28 10:18
+"""
+
+
+class Solution(object):
+    """
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    Example:
+    Given nums = [2, 7, 11, 15], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+
+    """
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(0, n):
+            j = i + 1
+            for j in range(j, n):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+
+nums = [3, 4]
+target = 7
+solution = Solution()
+print(solution.twoSum(nums, target))
+
+
